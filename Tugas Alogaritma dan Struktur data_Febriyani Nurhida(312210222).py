144b70d3a258a68ee1272ea43b98ca174ceb8750
Febriyaninurhida123 <sutarmiprihatna@gmail.com>
2022-10-10 20:24:48 +0700

    project membuat string

---
+++ Tugas Alogaritma dan Struktur data_Febriyani Nurhida(312210222).py
@@ -0,0 +1,21 @@
+#Bahasa yang digunakan adalah bahasa pemroraman python
+#Membuat a String
+#Dengan single Quotes
+Belajar = 'Dilema seorang mahasiswa'
+print("Ketika waktu Selasa tiba, jiwa dan dada rasanya mulai membara:")
+print(Belajar)
+#Membuat String
+#Dengan double Quotes
+Belajar = "Dikejar waktu absen pukul tujuh pagi pas serasa kompetisi perulangan "
+print("\nMengapa mau kujalani dengan pasrah: ")
+print(Belajar)
+#Membuat String 
+#Dengan tiga Quotes
+Belajar = '''Lalu kujawab"Karena masa depan bangsa ada di pundakku'''
+print(Belajar)
+#Creating String with triple
+#Quotes allows multiple lines
+Belajar = '''Kumau
+        Kuliah'''
+print("\nPada mimpuku dihadapan semesta: ")
+print(Belajar)
